55b030b12486e5e15941496b7844ef6a0a435f9c
Mark Wagner <mwag96@live.ca>
2022-05-23 11:38:49 -0600

    Add project files.

---
+++ RandomNumberList/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using static RandomNumberList.RandomNumberListGenerator;
+
+namespace RandomNumberList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int Start { get; set; } = 1;
+        public int End { get; set; } = 10000;
+
+        public ObservableCollection<int> Numbers { get; } = new ObservableCollection<int>();
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+            GetNumbers();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GetNumbers();
+        }
+
+        private void GetNumbers()
+        {
+            this.Cursor = Cursors.Wait;
+            Numbers.Clear();
+            GetRandomList(Start, End).ForEach(x => Numbers.Add(x));
+            this.Cursor = Cursors.Arrow;
+        }
+    }
+}
---
+++ RandomNumberList/RandomNumberListGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberList
+{
+    public static class RandomNumberListGenerator
+    {
+        private static Random random = new Random();
+
+        /// <summary>
+        /// Get list of unique integers in provided range (inclusive)
+        /// </summary>
+        /// <returns></
+        /// returns>
+        public static List<int> GetRandomList(int x, int y)
+        {
+            // Determine which provided value is higher or lower.
+            int lower = x < y ? x : y;
+            int upper = y > x ? y : x;
+
+            // Create the list. This ensures that there are no duplicate values.
+            List<int> list = new List<int>();
+            for (int i = lower; i <= upper; i++)
+            {
+                list.Add(i);
+            }
+
+            // Then, scramble the list.
+            list.Scramble();
+
+            // Return scrambled list.
+            return list;
+        } 
+
+        /// <summary>
+        /// Steps through the list and swaps every element with one random other element, with a cance to not swap values.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private static void Scramble<T>(this List<T> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int r = random.Next(-1, list.Count - 1);
+                
+                if (r != -1)
+                {
+                    list.Swap(i, r);
+                }
+                    
+            }
+        }
+
+        /// <summary>
+        /// Swap two elements in a list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        private static void Swap<T>(this List<T> l, int x, int y)
+        {
+            T tmp = l[x];
+            l[x] = l[y];
+            l[y] = tmp;
+        }
+    }
+}
